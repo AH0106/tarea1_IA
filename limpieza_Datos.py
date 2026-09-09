61e2da054b4284fb195374cdfbc490869c8d9fbb
Andres Hormazabal <andres.hormazabal_r@mail.udp.cl>
2026-09-09 19:22:56 -0300

    Añade .gitignore y actualiza limpieza de datos

--- limpieza_Datos.py
+++ limpieza_Datos.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
 
+
+
+
 df = pd.read_csv(f"credit_risk_dataset.csv")
 
 # df[...] : devuelve solo las filas donde el valor es TRUE. Es un filtro
@@ -26,6 +29,3 @@ df_limpio = df_limpio[df_limpio['person_emp_length'] <= df_limpio['person_age'] 
 
 print("Filas finales:" , len(df_limpio))
 print(df_limpio[['person_age', 'person_emp_length', 'person_income']].describe())
-
-
-    |   
